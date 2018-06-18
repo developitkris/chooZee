b402a8a29c18a9af55a89cfb8747fbe9f5e7aa22
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-18 11:12:01 -0700

    add drinks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DrinksComponent } from './drinks/drinks.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrinksComponent
   ],
   imports: [
     BrowserModule
