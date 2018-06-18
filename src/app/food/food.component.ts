5e42497a03b642c2517fbfc4b2534e7fbb43fb73
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-18 16:19:40 -0700

    add food questions

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
   styleUrls: ['./food.component.css']
 })
-export class FoodComponent implements OnInit {
+export class FoodComponent {
 
   constructor() { }
 
