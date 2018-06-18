d718ead0adc7787cffa9eecb65677107413a6078
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-18 14:28:17 -0700

    delete movie component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DrinksComponent } from './drinks/drinks.component';
 import { FoodComponent } from './food/food.component';
+import { MoviesComponent } from './movies/movies.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DrinksComponent,
-    FoodComponent
+    FoodComponent,
+    MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
