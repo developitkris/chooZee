d99f5f07e1ef2804c191b9545d4190d470d5a7cc
cj <charlesrhonejr@gmail.com>
2018-06-19 12:18:30 -0700

    Working API

--- src/app/movie-form/movie-form.component.ts
+++ src/app/movie-form/movie-form.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { MovieSearch } from '../movie.service';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-movie-form',
@@ -9,16 +10,33 @@ import { MovieSearch } from '../movie.service';
   providers: [ MovieSearch ]
 })
 
-export class MovieFormComponent {
-  movies: any[] = null;
-  noMovies: boolean=false;
-  constructor(private movieTitles = MovieSearch ) { }
-  getMovies(name: string) {
-    this.movies=null;
-    this.movieTitles.getActorByName(name).subscribe(response => {
-      if(response.json().movies.length > 0) {
-        this.movies = response.json();
-      }
-    });
+export class MovieFormComponent implements OnInit {
+  movie = null;
+  constructor(private http: Http, private movieTitle: MovieSearch ) { }
+
+  ngOnInit(){
+    this.getMovie();
   }
+  getMovie(){
+    console.log( this.http.get("https://api.themoviedb.org/3/search/movie?api_key=357e76b265bade398b205c7b05084ffa&query=Jack+Reacher"));
+    this.http.get("https://api.themoviedb.org/3/search/movie?api_key=357e76b265bade398b205c7b05084ffa&query=Jack+Reacher").subscribe(response => {
+      console.log(response.json());
+    })
+
+  }
+
 }
+
+// export class MovieFormComponent {
+//   movies: any[] = null;
+//   noMovies: boolean=false;
+//   constructor(private movieTitle: MovieSearch ) { }
+//   getMovies(name: string) {
+//     this.movies=null;
+//     this.movieTitle.getActorByName(name).subscribe(response => {
+//       if(response.json().movies.length > 0) {
+//         this.movies = response.json();
+//       }
+//     });
+//   }
+// }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -4,9 +4,11 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class MovieSearch {
-  
+
   constructor(private http: Http) { }
+
   getActorByName(name: string) {
-    return this.http.get(`https://api.themoviedb.org/3/movie/550?original_title=api_key=357e76b265bade398b205c7b05084ffa`)
+    return this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=357e76b265bade398b205c7b05084ffa&query=Jack+Reacher`)
+
   }
 }
